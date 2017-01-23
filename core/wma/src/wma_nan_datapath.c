OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Fix NDP non-auto responder mode
propagation from qcacld-2.0 to qcacld-3.0

In NDP non-auto responder mode, peer create happens despite
the response request status is indicated as failure from the
framework as the status of the NDP responder request is not
saved in the protocol engine. So, later when host receives
responder response from the firmware, host does not check
responder request status before sending peer create request.

Fix by sending ndp peer create request only if the firware request
a peer create by setting a flag in responder response message. This
way host does not need to save the responder request success/failure
status per NAN service. Firmware sets the peer create flag to true
only if the status flag in the responder request was indicated as
success.

Change-Id: I4fc8429e71a10829726990cf218720f6f99b085c
CRs-Fixed: 1086217

core/wma/src/wma_nan_datapath.c
@@ -520,8 +520,15 @@ static int wma_ndp_responder_rsp_event_handler(void *handle,
 	rsp.transaction_id = fixed_params->transaction_id;
 	rsp.reason = fixed_params->reason_code;
 	rsp.status = fixed_params->rsp_status;
+	rsp.create_peer = fixed_params->create_peer;
 	WMI_MAC_ADDR_TO_CHAR_ARRAY(&fixed_params->peer_ndi_mac_addr,
 				rsp.peer_mac_addr.bytes);
+
+	WMA_LOGE(FL("WMI_NDP_RESPONDER_RSP_EVENTID(0x%X) received. vdev_id: %d, peer_mac_addr: %pM,transaction_id: %d, status_code %d, reason_code: %d, create_peer: %d"),
+			WMI_NDP_RESPONDER_RSP_EVENTID, rsp.vdev_id,
+			rsp.peer_mac_addr.bytes, rsp.transaction_id,
+			rsp.status, rsp.reason, rsp.create_peer);
+
 	pe_msg.bodyptr = &rsp;
 	pe_msg.type = SIR_HAL_NDP_RESPONDER_RSP;
 	return wma_handle->pe_ndp_event_handler(wma_handle->mac_context,

nekoshirro/platform_kernel_xiaomi_marble-modules

Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

core/wma/src/wma_nan_datapath.c
@@ -523,8 +523,15 @@ static int wma_ndp_responder_rsp_event_handler(void *handle,
 	rsp.transaction_id = fixed_params->transaction_id;
 	rsp.reason = fixed_params->reason_code;
 	rsp.status = fixed_params->rsp_status;
+	rsp.create_peer = fixed_params->create_peer;
 	WMI_MAC_ADDR_TO_CHAR_ARRAY(&fixed_params->peer_ndi_mac_addr,
 				rsp.peer_mac_addr.bytes);
+
+	WMA_LOGD(FL("WMI_NDP_RESPONDER_RSP_EVENTID(0x%X) received. vdev_id: %d, peer_mac_addr: %pM,transaction_id: %d, status_code %d, reason_code: %d, create_peer: %d"),
+			WMI_NDP_RESPONDER_RSP_EVENTID, rsp.vdev_id,
+			rsp.peer_mac_addr.bytes, rsp.transaction_id,
+			rsp.status, rsp.reason, rsp.create_peer);
+
 	pe_msg.bodyptr = &rsp;
 	pe_msg.type = SIR_HAL_NDP_RESPONDER_RSP;
 	return wma_handle->pe_ndp_event_handler(wma_handle->mac_context,