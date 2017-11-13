OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Add support to enable/disable FILS runtime
Currently, FILS feature is enabled with ini parameter
[is_fils_enabled] at init time. This change adds support
to enable disable FILS from vendor command.

Change-Id: I0c84d777a6259c96233a4777f184ddf6f7bc58af
CRs-Fixed: 2121214

core/hdd/src/wlan_hdd_cfg.c
@@ -8720,6 +8720,8 @@ QDF_STATUS hdd_set_sme_config(hdd_context_t *pHddCtx)
 	if (pHddCtx->config->is_fils_enabled)
 		smeConfig->csrConfig.enable_bcast_probe_rsp =
 			pHddCtx->config->enable_bcast_probe_rsp;
+	smeConfig->csrConfig.is_fils_enabled =
+			pHddCtx->config->is_fils_enabled;
 	smeConfig->csrConfig.qcn_ie_support =
 			pHddCtx->config->qcn_ie_support;
 	smeConfig->csrConfig.fils_max_chan_guard_time =

nekoshirro/platform_kernel_xiaomi_marble-modules

Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

core/hdd/src/wlan_hdd_cfg.c
@@ -8857,6 +8857,8 @@ QDF_STATUS hdd_set_sme_config(struct hdd_context *hdd_ctx)
 			hdd_ctx->config->rx_aggregation_size;
 	smeConfig->csrConfig.enable_bcast_probe_rsp =
 			hdd_ctx->config->enable_bcast_probe_rsp;
+	smeConfig->csrConfig.is_fils_enabled =
+			hdd_ctx->config->is_fils_enabled;
 	smeConfig->csrConfig.qcn_ie_support =
 			hdd_ctx->config->qcn_ie_support;
 	smeConfig->csrConfig.fils_max_chan_guard_time =