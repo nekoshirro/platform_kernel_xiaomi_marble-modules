UtsavBalar1231/kernel_xiaomi_raphael

qcacmn: Flush srng tp and hp only for flush event
Currently after runtime resume all SW2TCL data and reo cmd
srng rings hp and tp value are flushed. In case of IPA
offload case SW2TCL3 righ hp value will be updated by IPA
and not by host. In case of runtime pm enable host is
setting the value to zero as part of runtime resume which
results in incorrect hp value of SW2TCL3. As part of this
change set flush event for rings which are accessed by host
during link down state and after runtime resume flush the
rings for which flush event is set.

Change-Id: I5c9afa708277cf3a6e6d5ef99447bc21f88cfdcf
CRs-Fixed: 2514621

dp/wifi3.0/dp_tx.c
@@ -1538,6 +1538,8 @@ fail_return:
 		hif_pm_runtime_put(soc->hif_handle);
 	} else {
 		hal_srng_access_end_reap(soc->hal_soc, hal_srng);
+		hal_srng_set_event(hal_srng, HAL_SRNG_FLUSH_EVENT);
+		hal_srng_inc_flush_cnt(hal_srng);
 	}
 
 	return nbuf;
@@ -1709,6 +1711,8 @@ done:
 		hif_pm_runtime_put(soc->hif_handle);
 	} else {
 		hal_srng_access_end_reap(soc->hal_soc, hal_srng);
+		hal_srng_set_event(hal_srng, HAL_SRNG_FLUSH_EVENT);
+		hal_srng_inc_flush_cnt(hal_srng);
 	}
 
 	return nbuf;

hal/wifi3.0/hal_reo.c
@@ -670,7 +670,6 @@ inline int hal_reo_cmd_update_rx_queue(void *reo_ring, struct hal_soc *soc,
 {
 	uint32_t *reo_desc, val;
 	struct hal_reo_cmd_update_queue_params *p;
-	struct hal_srng *srng = (struct hal_srng *)reo_ring;
 
 	p = &cmd->u.upd_queue_params;
 
@@ -872,7 +871,8 @@ inline int hal_reo_cmd_update_rx_queue(void *reo_ring, struct hal_soc *soc,
 		hif_pm_runtime_put(soc->hif_handle);
 	} else {
 		hal_srng_access_end_reap(soc, reo_ring);
-		srng->needs_flush++;
+		hal_srng_set_event(reo_ring, HAL_SRNG_FLUSH_EVENT);
+		hal_srng_inc_flush_cnt(reo_ring);
 	}
 
 	val = reo_desc[CMD_HEADER_DW_OFFSET];

nekoshirro/platform_kernel_xiaomi_marble-modules

Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

dp/wifi3.0/dp_tx.c
@@ -1581,6 +1581,8 @@ fail_return:
 		hif_pm_runtime_put(soc->hif_handle);
 	} else {
 		hal_srng_access_end_reap(soc->hal_soc, hal_ring_hdl);
+		hal_srng_set_event(hal_ring_hdl, HAL_SRNG_FLUSH_EVENT);
+		hal_srng_inc_flush_cnt(hal_ring_hdl);
 	}
 
 	return nbuf;
@@ -1749,6 +1751,8 @@ done:
 		hif_pm_runtime_put(soc->hif_handle);
 	} else {
 		hal_srng_access_end_reap(soc->hal_soc, hal_ring_hdl);
+		hal_srng_set_event(hal_ring_hdl, HAL_SRNG_FLUSH_EVENT);
+		hal_srng_inc_flush_cnt(hal_ring_hdl);
 	}
 
 	return nbuf;

hal/wifi3.0/hal_reo.c
@@ -686,7 +686,6 @@ inline int hal_reo_cmd_update_rx_queue(hal_ring_handle_t hal_ring_hdl,
 	struct hal_soc *hal_soc = (struct hal_soc *)hal_soc_hdl;
 	uint32_t *reo_desc, val;
 	struct hal_reo_cmd_update_queue_params *p;
-	struct hal_srng *srng = (struct hal_srng *)hal_ring_hdl;
 
 	p = &cmd->u.upd_queue_params;
 
@@ -888,7 +887,8 @@ inline int hal_reo_cmd_update_rx_queue(hal_ring_handle_t hal_ring_hdl,
 		hif_pm_runtime_put(hal_soc->hif_handle);
 	} else {
 		hal_srng_access_end_reap(hal_soc_hdl, hal_ring_hdl);
-		srng->needs_flush++;
+		hal_srng_set_event(hal_ring_hdl, HAL_SRNG_FLUSH_EVENT);
+		hal_srng_inc_flush_cnt(hal_ring_hdl);
 	}
 
 	val = reo_desc[CMD_HEADER_DW_OFFSET];