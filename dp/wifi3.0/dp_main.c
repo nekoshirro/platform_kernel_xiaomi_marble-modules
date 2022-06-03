MiCode/vendor_qcom_opensource_wlan

qcacmn: Add configurable threshold values
Introduce configurable threshold values to
increment MPDU retry count by transmit_cnt / threshold.

Change-Id: I1c039ff76854e18bea5a8d99d171936557d1c6bf
CRs-Fixed: 3210735

dp/wifi3.0/dp_main.c
@@ -7088,6 +7088,12 @@ static QDF_STATUS dp_txrx_peer_attach(struct dp_soc *soc, struct dp_peer *peer)
 	txrx_peer->vdev = peer->vdev;
 	pdev = peer->vdev->pdev;
 
+	/* Initialize MPDU success count with retry update thresholds */
+	txrx_peer->mpdu_retry_threshold_1 =
+		soc->wlan_cfg_ctx->mpdu_retry_threshold_1;
+	txrx_peer->mpdu_retry_threshold_2 =
+		soc->wlan_cfg_ctx->mpdu_retry_threshold_2;
+
 	DP_STATS_INIT(txrx_peer);
 
 	dp_wds_ext_peer_init(txrx_peer);

dp/wifi3.0/dp_tx.c
@@ -4062,7 +4062,7 @@ static inline void
 dp_tx_update_peer_extd_stats(struct hal_tx_completion_status *ts,
 			     struct dp_txrx_peer *txrx_peer)
 {
-	uint8_t mcs, pkt_type;
+	uint8_t mcs, pkt_type, retry_threshold;
 
 	mcs = ts->mcs;
 	pkt_type = ts->pkt_type;
@@ -4109,9 +4109,23 @@ dp_tx_update_peer_extd_stats(struct hal_tx_completion_status *ts,
 	if (ts->first_msdu) {
 		DP_PEER_EXTD_STATS_INCC(txrx_peer, tx.retries_mpdu, 1,
 					ts->transmit_cnt > 1);
+		switch (ts->bw) {
+		case 0: /* 20Mhz */
+		case 1: /* 40Mhz */
+		case 2: /* 80Mhz */
+			retry_threshold = txrx_peer->mpdu_retry_threshold_1;
+			break;
+		default: /* 160Mhz */
+			retry_threshold = txrx_peer->mpdu_retry_threshold_2;
+			break;
+		}
+		if (!retry_threshold)
+			return;
+
 		DP_PEER_EXTD_STATS_INCC(txrx_peer, tx.mpdu_success_with_retries,
-					qdf_do_div(ts->transmit_cnt, DP_RETRY_COUNT),
-					ts->transmit_cnt > DP_RETRY_COUNT);
+					qdf_do_div(ts->transmit_cnt,
+						   retry_threshold),
+					ts->transmit_cnt > retry_threshold);
 	}
 }
 #else

nekoshirro/platform_kernel_xiaomi_marble-modules

Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

dp/wifi3.0/dp_main.c
@@ -7009,6 +7009,12 @@ dp_peer_create_wifi3(struct cdp_soc_t *soc_hdl, uint8_t vdev_id,
 		}
 	}
 
+	/* Initialize MPDU success count with retry update thresholds */
+	peer->mpdu_retry_threshold_1 =
+		soc->wlan_cfg_ctx->mpdu_retry_threshold_1;
+	peer->mpdu_retry_threshold_2 =
+		soc->wlan_cfg_ctx->mpdu_retry_threshold_2;
+
 	/*
 	 * Allocate peer extended stats context. Fall through in
 	 * case of failure as its not an implicit requirement to have

dp/wifi3.0/dp_tx.c
@@ -3729,7 +3729,7 @@ static inline void
 dp_tx_update_acked_rate_stats(struct dp_peer *peer,
 			      struct hal_tx_completion_status *ts)
 {
-	uint8_t mcs, pkt_type;
+	uint8_t mcs, pkt_type, retry_threshold;
 
 	mcs = ts->mcs;
 	pkt_type = ts->pkt_type;
@@ -3764,9 +3764,21 @@ dp_tx_update_acked_rate_stats(struct dp_peer *peer,
 	DP_STATS_INCC(peer, tx.retries, 1, ts->transmit_cnt > 1);
 	if (ts->first_msdu) {
 		DP_STATS_INCC(peer, tx.retries_mpdu, 1, ts->transmit_cnt > 1);
-		DP_STATS_INCC(peer, tx.mpdu_success_with_retries,
-			      qdf_do_div(ts->transmit_cnt, DP_RETRY_COUNT),
-			      ts->transmit_cnt > DP_RETRY_COUNT);
+		switch (ts->bw) {
+		case 0: /* 20Mhz */
+		case 1: /* 40Mhz */
+		case 2: /* 80Mhz */
+			retry_threshold = peer->mpdu_retry_threshold_1;
+			break;
+		default: /* 160Mhz */
+			retry_threshold = peer->mpdu_retry_threshold_2;
+			break;
+		}
+		if (retry_threshold)
+			DP_STATS_INCC(peer, tx.mpdu_success_with_retries,
+				      qdf_do_div(ts->transmit_cnt,
+						 retry_threshold),
+				      ts->transmit_cnt > retry_threshold);
 	}
 	peer->stats.tx.last_tx_ts = qdf_system_ticks();
 }